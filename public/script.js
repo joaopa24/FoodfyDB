d88ef5ba143d7e9ecf542ae96b4382153d3f07ca
joknz21 <joaokn129@gmail.com>
2021-01-13 12:44:05 -0300

    fix recipe script

--- public/script.js
+++ public/script.js
@@ -6,12 +6,12 @@ const contenthide = document.querySelector('.content_text_hide')
 for(let card of cards){
     card.addEventListener("click",function(){
     Modaloverlay.classList.add("active")
-    const id = card.getAttribute("id")
+    const image = card.getAttribute("image")
     const title = card.querySelector('.card_title').textContent
     const description = card.querySelector('.card_description').textContent
     modal.querySelector('h1').innerHTML = title
     modal.querySelector('p').innerHTML = description
-    Modaloverlay.querySelector('img').src = `/assets/${id}.png`
+    Modaloverlay.querySelector('img').src = `${image}`
     document.querySelector('.details').addEventListener('click', function(){
         window.location.href = `/Receitas/${id}`
     })
