e32bbd393bbfdb630ae6b6ad585a84460467c93a
joknz21 <joaokn129@gmail.com>
2021-01-25 22:47:24 -0300

    refactor style add

--- public/script_admin.js
+++ public/script_admin.js
@@ -1,10 +1,10 @@
-const ingredients = document.querySelector(".ingredients[]")
+const ingredients = document.querySelector(".ingredients")
 const button = document.querySelector('.add_ingredient')
-const preparations = document.querySelector(".preparations[]w")
+const preparations = document.querySelector(".preparations")
 const button_preparation = document.querySelector(".add_preparation")
 
 button.addEventListener("click", function (){
-    const fields_Ingredients = document.querySelectorAll(".ingredient[]")
+    const fields_Ingredients = document.querySelectorAll(".ingredient")
     const New_field = fields_Ingredients[fields_Ingredients.length - 1].cloneNode(true)
     
     if(New_field.children[0].value == ""){
@@ -16,7 +16,7 @@ button.addEventListener("click", function (){
 }
 )
 button_preparation.addEventListener("click", function (){
-    const fields_preparations = document.querySelectorAll(".preparation[]")
+    const fields_preparations = document.querySelectorAll(".preparation")
     const New_field_preparation = fields_preparations[fields_preparations.length - 1].cloneNode(true)
     
     if(New_field_preparation.children[0].value == ""){
