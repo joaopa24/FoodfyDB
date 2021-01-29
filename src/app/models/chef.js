e406d92a7217dc060b5ccee583944f25ce9d461e
joknz21 <joaokn129@gmail.com>
2021-01-28 22:16:52 -0300

    add create model chef

--- src/app/models/chef.js
+++ src/app/models/chef.js
@@ -7,6 +7,25 @@ module.exports = {
 
            callback(results.rows)
        })   
+    },
+    create(data , callback){
+        const query = `
+        INSERT INTO Chefs(
+            avatar,
+            nome_chef
+        ) VALUES ($1 , $2)
+        `
+        
+        const values = [
+            data.avatar,
+            data.nome_chef
+        ]
+
+        db.query(query , values, function(err , results){
+            if(err) throw `${err}`
+            
+            callback(results.rows[0])
+        })
     }
 
 }
